6b796768530b91959e72552b7d4a228d17ff5f93
EricFilipe <ericfilipe125@gmail.com>
2023-10-02 14:00:21 -0300

    integration changes

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express()
 const cors = require('cors');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "postgres://EricFilipe:0AlT2WqxwMjI@ep-cold-haze-53725092.us-east-2.aws.neon.tech/neondb");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST");
     next();
