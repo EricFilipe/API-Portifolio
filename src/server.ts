ef15dcfbc252e32ea2315f158ec8181d9e38f1c2
EricFilipe <ericfilipe125@gmail.com>
2023-10-03 11:39:58 -0300

    url change

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express()
 const cors = require('cors');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://api-portifolio-imu2s1kaq-ericfilipe.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST");
     next();
